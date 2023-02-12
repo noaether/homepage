3d419efa437cf537b05d4fc3dd0ecadde9231d81
noae <67566122+noaether@users.noreply.github.com>
2023-02-12 15:34:48 -0500

    Update bookmarks.js

--- bookmarks.js
+++ bookmarks.js
@@ -1,47 +1,40 @@
 // Note: having length != 4 will mess with layout based on how the site is styled
 const bookmarks = [
   {
-    title: "Daily",
+    title: "planning",
     links: [
-      { name: "Inbox", url: "https://inbox.google.com" },
-      { name: "GitHub", url: "https://github.com" },
-      { name: "Drive", url: "https://drive.google.com" },
+      { name: "calendar", url: "https://calendar.google.com/calendar/u/0/r/week?autorel=1" },
+      { name: "studyo", url: "https://studyo.app/c/63093be8b87df182ae983156/a/63093be8b87df182ae98315f/agenda/daily" },
+      { name: "keep", url: "https://keep.google.com/u/0/" },
     ],
   },
   {
     title: "Media",
     links: [
-      { name: "Youtube", url: "https://youtube.com" },
-      { name: "Netflix", url: "https://netflix.com" },
-      { name: "Crunchyroll", url: "https://crunchyroll.com" },
-      {
-        name: "Amazon Prime",
-        url: "https://www.amazon.com/Amazon-Video",
-      },
+      { name: "youtube", url: "https://youtube.com" },
+      { name: "music", url: "https://music.youtube.com" },
+      { name: "netflix", url: "https://www.netflix.com/browse" },
+      { name: "instagram", url: "https://www.instagram.com/direct/inbox/" },
     ],
   },
   {
-    title: "Reddit",
+    title: "servers",
     links: [
-      { name: "/r/overwatch", url: "https://reddit.com/r/overwatch" },
-      {
-        name: "/r/pcmasterrace",
-        url: "https://reddit.com/r/pcmasterrace",
-      },
-      { name: "/r/me_irl", url: "https://reddit.com/r/me_irl" },
-      {
-        name: "/r/battlestations",
-        url: "https://reddit.com/r/battlestations",
-      },
-      { name: "/r/unixporn", url: "https://reddit.com/r/unixporn" },
-      { name: "/r/news", url: "https://reddit.com/r/news" },
+      { name: "webmin salt", url: "https://salt.noae.gay:10000/sysinfo.cgi?xnavigation=1" },
+      { name: "webmin pepper", url: "https://pepper.noae.gay:10000/sysinfo.cgi?xnavigation=1" },
+      { name: "ionos salt", url: "https://cloudpanel.ionos.ca/panel?ionospanelid=ionos3dbcf26a-8a6a-4b94-985f-d94df008044710054" },
+      { name: "ionos pepper", url: "https://cloudpanel.ionos.ca/panel/app/global?ionospanelid=ionos976b6e2a-7efb-48e4-9f56-43668c14ef7410184#/servers" },
+      { name: "cloudflare", url: "https://dash.cloudflare.com/73ce49d19a6761e88426e74dc8cf7fd8" },
     ],
   },
   {
-    title: "Social",
+    title: "school",
     links: [
-      { name: "Twitter", url: "https://twitter.com" },
-      { name: "Facebook", url: "https://facebook.com" },
+      { name: "office", url: "https://www.office.com/?auth=2" },
+      { name: "onedrive", url: "https://csdma-my.sharepoint.com/personal/st-ongen_csdmedu_ca//_layouts/15/onedrive.aspx?login_hint=st%2Dongen%40csdmedu%2Eca" },
+      { name: "mathsolver", url: "https://mathsolver.microsoft.com/en" },
+      { name: "teams", url: "https://teams.microsoft.com/_" },
+      { name: "translate", url: "https://translate.google.com/?hl=fr" },
     ],
   },
 ];
