d3e02ff82231be86de512e09e5248fd6c1d17a4a
noae <67566122+noaether@users.noreply.github.com>
2023-05-11 20:55:26 +0000

    removed two bookmarks

--- bookmarks.js
+++ bookmarks.js
@@ -20,8 +20,6 @@ const bookmarks = [
   {
     title: "servers",
     links: [
-      { name: "webmin salt", url: "https://salt.noae.gay:10000/sysinfo.cgi?xnavigation=1" },
-      { name: "webmin pepper", url: "https://pepper.noae.gay:10000/sysinfo.cgi?xnavigation=1" },
       { name: "ionos salt", url: "https://cloudpanel.ionos.ca/panel?ionospanelid=ionos3dbcf26a-8a6a-4b94-985f-d94df008044710054" },
       { name: "ionos pepper", url: "https://cloudpanel.ionos.ca/panel/app/global?ionospanelid=ionos976b6e2a-7efb-48e4-9f56-43668c14ef7410184#/servers" },
       { name: "cloudflare", url: "https://dash.cloudflare.com/73ce49d19a6761e88426e74dc8cf7fd8" },
@@ -34,7 +32,7 @@ const bookmarks = [
       { name: "onedrive", url: "https://csdma-my.sharepoint.com/personal/st-ongen_csdmedu_ca//_layouts/15/onedrive.aspx?login_hint=st%2Dongen%40csdmedu%2Eca" },
       { name: "mathsolver", url: "https://mathsolver.microsoft.com/en" },
       { name: "teams", url: "https://teams.microsoft.com/_" },
-      { name: "translate", url: "https://translate.google.com/?hl=fr" },
+      { name: "translate", url: "https://translate.google.com/?hl=fr" }, // hello :))
     ],
   },
 ];
